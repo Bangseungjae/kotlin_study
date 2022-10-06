c64bc336348fa9d6f764ea5905c2045c482333cc
Bangseungjae <tmdwo5825@naver.com>
2022-10-06 22:18:38 +0900

    sealed class

---
+++ src/main/kotlin/com/example/kotlin_study/study/two/4_SealedClass.kt
@@ -0,0 +1,60 @@
+package com.example.kotlin_study.study.two
+
+sealed class Developer {
+
+    abstract val name: String
+    abstract fun code(language: String)
+}
+
+data class BackendDeveloper(override val name: String) : Developer() {
+    override fun code(language: String) {
+        println("저는 백엔드 개발자입니다 ${language}를 사용할 수 있습니다.")
+    }
+}
+
+data class FrontendDeveloper(override val name: String) : Developer() {
+    override fun code(language: String) {
+        println("저는 프론트엔드 개발자입니다 ${language}를 사용할 수 있습니다.")
+    }
+}
+
+data class AndroidDeveloper(override val name: String) : Developer() {
+    override fun code(language: String) {
+        println("저는 안드로이드 개발자입니다 ${language}를 사용할 수 있습니다.")
+    }
+}
+
+object OtherDeveloper : Developer() {
+    override val name: String = "익명"
+
+    override fun code(language: String) {
+        TODO("Not yet implemented")
+    }
+}
+
+object DeveloperPool {
+    val pool = mutableMapOf<String, Developer>()
+
+    fun add(developer: Developer) = when (developer) {
+        is BackendDeveloper -> pool[developer.name] = developer
+        is FrontendDeveloper -> pool[developer.name] = developer
+        is AndroidDeveloper -> pool[developer.name] = developer
+        is OtherDeveloper -> println("지원하지 않는 개발자종류입니다")
+
+    }
+    fun get(name: String) = pool[name]
+}
+
+fun main() {
+    val backendDeveloper = BackendDeveloper(name = "토니")
+    DeveloperPool.add(backendDeveloper)
+
+    val frontendDeveloper = FrontendDeveloper(name = "카즈야")
+    DeveloperPool.add(frontendDeveloper)
+
+    val androidDeveloper = AndroidDeveloper(name = "안드로")
+
+    println(DeveloperPool.get("토니"))
+    println(DeveloperPool.get("카즈야"))
+    println(DeveloperPool.get("안드"))
+}
