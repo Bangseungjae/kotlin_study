b53d80c66dd6b9d64cec0843c641decdb571999b
Bangseungjae <tmdwo5825@naver.com>
2022-10-07 18:09:34 +0900

    스코프 함수

--- src/main/kotlin/com/example/kotlin_study/study/two/9_with.kt
+++ src/main/kotlin/com/example/kotlin_study/study/two/9_with.kt
@@ -10,14 +10,14 @@ fun main() {
 
 
     val connected: Boolean = DatabaseClient().run {
-        url = "localhost3036"
+        url = "localhost:3036"
         username = "mysql"
         password = "1234"
         connect()
     }
 
     val result: Boolean = with(DatabaseClient()) {
-        url = "localhost3036"
+        url = "localhost:3036"
         username = "mysql"
         password = "1234"
         connect()
